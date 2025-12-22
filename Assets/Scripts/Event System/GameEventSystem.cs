f11d88495b1ec69a541e7e3f18ce74256caae2ce
BalbiCreativeLab <159083570+BalbiCreativeLab@users.noreply.github.com>
2025-12-22 16:36:37 +0100

    Update GameEventSystem.cs

--- Assets/Scripts/Event System/GameEventSystem.cs
+++ Assets/Scripts/Event System/GameEventSystem.cs
@@ -2,45 +2,12 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameEvent
-{
-}
+public class GameEvent { }
 
 public class GameEventData
 {
     public GameObject instigator;
 }
-
-//public static class GameEventSystem
-//{
-//    static List<GameEvent> events;
-
-//    public static void AddEvent(GameEvent gameEvent)
-//    {
-//        if(!events.Contains(gameEvent))
-//            events.Add(gameEvent);
-//    }
-//    public static void RemoveEvent(GameEvent gameEvent)
-//    {
-//        if (events.Contains(gameEvent))
-//            events.Remove(gameEvent);
-//    }
-//    public static void Subscribe(GameEvent gameEvent, Action<GameEventData> listener) {
-//        if (!events.Contains(gameEvent))
-//            gameEvent.listeners += listener;
-//    }
-//    public static void Unsubscribe(GameEvent gameEvent, Action<GameEventData> listener) {
-//        if (events.Contains(gameEvent))
-//            gameEvent.listeners -= listener;
-//    }
-
-//    public static void TriggerEvent(GameEvent gameEvent, GameEventData data)
-//    {
-//        if (events.Contains(gameEvent))
-//            gameEvent.Invoke(data);
-//    }
-//}
-
 
 public static class GameEventSystem
 {
